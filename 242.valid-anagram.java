93e31046a5e8f3bb19ae690c2c3eaea51f474613
geostorm <77898333+sai-Ga@users.noreply.github.com>
2024-10-28 14:59:02 +0530

    Add files via upload

---
+++ 242.valid-anagram.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
